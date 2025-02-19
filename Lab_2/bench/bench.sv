42b5cdd344670d851d56a7c7278e034ef05e81d1
Zainhaider-512 <zainhaider2772005@gmail.com>
2025-02-20 00:21:30 +0500

    Add files via upload

--- Lab_2/bench/bench.sv
+++ Lab_2/bench/bench.sv
@@ -1,37 +1,60 @@
 module bench;
-logic a1;
-logic b1;
-logic c1;
-logic x1;
-logic y1;
+logic [1:0]a1;
+logic [1:0]b1;
+logic r1;
+logic g1;
+logic bl1;
 localparam period = 10;
-rtl UUT(
+dsd4 UUT(
 .a(a1),
 .b(b1),
-.c(c1),
-.x(x1),
-.y(y1)
+.r(r1),
+.g(g1),
+.bl(bl1)
 );
 initial //initial block executes only once
 begin
 // Provide different combinations of the inputs to check validity of code
-a1 = 0; b1 = 0; c1 = 0;
+a1[0] = 0; a1[1] = 0; b1[0] = 0; b1[1] = 0;
 #period;
-a1 = 0; b1 = 0; c1 = 1;
+a1[0] = 0; a1[1] = 0; b1[0] = 0; b1[1] = 1;
 #period;
-a1 = 0; b1 = 1; c1 = 0;
+a1[0] = 0; a1[1] = 0; b1[0] = 1; b1[1] = 0;
 #period;
-a1 = 0; b1 = 1; c1 = 1;
+a1[0] = 0; a1[1] = 0; b1[0] = 1; b1[1] = 1;
 #period;
-a1 = 1; b1 = 0; c1 = 0;
+a1[0] = 0; a1[1] = 1; b1[0] = 0; b1[1] = 0;
 #period;
-a1 = 1; b1 = 0; c1 = 1;
+a1[0] = 0; a1[1] = 1; b1[0] = 0; b1[1] = 1;
 #period;
-a1 = 1; b1 = 1; c1 = 0;
+a1[0] = 0; a1[1] = 1; b1[0] = 1; b1[1] = 0;
 #period;
-a1 = 1; b1 = 1; c1 = 1;
+a1[0] = 0; a1[1] = 1; b1[0] = 1; b1[1] = 1;
+#period;
+a1[0] = 1; a1[1] = 0; b1[0] = 0; b1[1] = 0;
+#period;
+a1[0] = 1; a1[1] = 0; b1[0] = 0; b1[1] = 1;
+#period;
+a1[0] = 1; a1[1] = 0; b1[0] = 1; b1[1] = 0;
+#period;
+a1[0] = 1; a1[1] = 0; b1[0] = 1; b1[1] = 1;
+#period;
+a1[0] = 1; a1[1] = 1; b1[0] = 0; b1[1] = 0;
+#period;
+a1[0] = 1; a1[1] = 1; b1[0] = 0; b1[1] = 1;
+#period;
+a1[0] = 1; a1[1] = 1; b1[0] = 1; b1[1] = 0;
+#period;
+a1[0] = 1; a1[1] = 1; b1[0] = 1; b1[1] = 1;
 #period;
 $stop;
 end
+
+initial
+begin
+    $monitor("a0= %d, a1= %d, b0= %d, b1= %d, r= %d, g= %d, bl= %d", 
+             a1[0], a1[1], b1[0], b1[1], r1, g1, bl1);
+end
+
 
 endmodule
